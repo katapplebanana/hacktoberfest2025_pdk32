cfdd80ce5e3a9c3c029acadd37a9e345bc0b1ce2
katapplebanana <k.haena.park@gmail.com>
2025-10-20 23:41:19 -0700

    Update simple_functions.py

--- algorithm_study/simple_functions.py
+++ algorithm_study/simple_functions.py
@@ -1,9 +1,13 @@
 def add(a, b):
 	return a+b
 
+# add a substraction function
+def subtract(a, b):
+	return a-b
 
 def test_suite():
 	assert add(1, 2)==3
+	assert subtract(2, 1)==1
 	print("passed all tests")
 
 
