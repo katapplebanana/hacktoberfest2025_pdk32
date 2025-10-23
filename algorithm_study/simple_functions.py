6a41e0c477b857bc656cc9e76456854f14eee130
katapplebanana <k.haena.park@gmail.com>
2025-10-22 19:58:10 -0700

    Added contributor information. Added new multiplying function.

--- algorithm_study/simple_functions.py
+++ algorithm_study/simple_functions.py
@@ -10,12 +10,21 @@ def divide(a, b):
 	assert (b!=0)
 	return a/b
 
+# add a multiplication function
+def multiply(a,b):
+	return a*b
+
+# test suite
 def test_suite():
 	assert add(1, 2)==3
 	assert subtract(2, 1)==1
 	assert divide(6, 2)==3
+	assert multiply(2,3)==6
 	print("passed all tests")
 
 
+#############################
+# main function
+#############################
 if __name__ == "__main__":
 	test_suite()
